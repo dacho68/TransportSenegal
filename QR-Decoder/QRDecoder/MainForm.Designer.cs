c4f4e92a710380618d068c53ba7923837a28a55d
dacho68 <dacho68@gmail.com>
2018-02-05 00:13:02 -0500

    init

---
+++ QR-Decoder/QRDecoder/MainForm.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace QRDecoder
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.result = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.speed = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 577);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "System Status";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // result
+            // 
+            this.result.Location = new System.Drawing.Point(16, 597);
+            this.result.Margin = new System.Windows.Forms.Padding(4);
+            this.result.Multiline = true;
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(479, 37);
+            this.result.TabIndex = 3;
+            this.result.Text = "Not Ready";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 650);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Speed (ms):";
+            // 
+            // speed
+            // 
+            this.speed.Location = new System.Drawing.Point(100, 646);
+            this.speed.Margin = new System.Windows.Forms.Padding(4);
+            this.speed.Name = "speed";
+            this.speed.Size = new System.Drawing.Size(111, 22);
+            this.speed.TabIndex = 6;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::QRDecoder.Properties.Resources.iTechFlare_sideway;
+            this.pictureBox2.Location = new System.Drawing.Point(14, 13);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(679, 146);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 13;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(135, 176);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(431, 398);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.WaitOnLoad = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 830);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.speed);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Transport Senegal";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox result;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox speed;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ QR-Decoder/README.md
@@ -0,0 +1,10 @@
+.Net-QR-Decoder using Webcam
+===============
+
+The notion behind developing a sample C# code is to illustrate the basic steps need to be taken for .Net developers to build a simple QR Decoder, and show the developers how to use the famous zxing library to decode 2D barcode library in realtime, either by capturing pictures from a webcam or by uploading a static 2D barcode picture.
+The code uses AForge.dll as a Webcam video streaming library. Make sure to add AForge as a reference if its somehow was removed. By default this library should be included inside the .Net project.
+
+Here is the official page [Documentation](http://www.itechflare.com/main/blog/c-program-for-2d-barcode-decoding/ "Code page").
+----------------------------
+
+![Sample] (http://www.itechflare.com/main/wp-content/uploads/2014/03/2dbarcode.png)
